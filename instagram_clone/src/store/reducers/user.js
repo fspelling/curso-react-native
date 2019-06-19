6c25816ff2d6664a41892429954305ef4c3156b2
Fernando <f.l.spelling@gmail.com>
2019-06-19 00:10:24 -0300

    .

---
+++ instagram_clone/src/store/reducers/user.js
@@ -0,0 +1,22 @@
+import { USER_LOGGED_IN, USER_LOGGED_OUT } from '../../actionTypes';
+
+const initialState = { name: null, email: null };
+
+export default reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case USER_LOGGED_IN:
+            return {
+                ...state,
+                name: action.payload.name,
+                email: action.payload.email
+            };
+        case USER_LOGGED_OUT:
+            return {
+                ...state,
+                name: null,
+                email: null
+            };
+        default:
+            return state
+    }
+}
---
+++ instagram_clone/src/store/storeConfig.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from 'redux';
+import userReducer from './reducers/user';
+
+const reducers = combineReducers({
+    user: userReducer
+});
+
+export default storeConfig = () => {
+    return createStore(reducers);
+}
