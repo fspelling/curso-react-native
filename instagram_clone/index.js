4c887ed4470a2a2c78e5054847d09e0ec9e8c432
Fernando <f.l.spelling@gmail.com>
2019-06-19 00:44:43 -0300

    .

--- instagram_clone/index.js
+++ instagram_clone/index.js
@@ -1,5 +1,18 @@
-import {AppRegistry} from 'react-native';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { AppRegistry } from 'react-native';
 import Navigator from './src/Navigator';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import storeConfig from './src/store/storeConfig';
 
-AppRegistry.registerComponent(appName, () => Navigator);
+const store = storeConfig();
+
+const Redux = () => {
+    return (
+        <Provider store={store}>
+            <Navigator />
+        </Provider>
+    );
+}
+
+AppRegistry.registerComponent(appName, () => Redux);
