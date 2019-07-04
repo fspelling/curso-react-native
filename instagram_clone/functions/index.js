ff0e816c1b20ad3560d0be19af9fd507c544f84d
Fernando Spelling <fernando.spelling@linx.com.br>
2019-07-04 15:10:53 -0300

    .

--- instagram_clone/functions/index.js
+++ instagram_clone/functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
+const fs = require('fs');
+const uuid = require('uuid-v4');
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage({ projectId: 'lambe-f0a43', keyFilename: 'lambe-f0a43.json' });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+ exports.uploadImage = functions.https.onRequest((request, response) => {
+     cors(request, response, () => {
+     });
+ });
